5524c10eba7b10619a2e36feba000a39bcfad033
Carson S. Christian <cc@amplego.com>
2015-11-10 13:04:12 -0500

    update device tests

--- test/device.js
+++ test/device.js
@@ -22,42 +22,43 @@ describe('Device', function () {
 			before(function (done) {
 				device = new Device(target.host, target.port, done);
 			});
-			it('should have a valid [info] property', function () {
+			it('should have a valid [info] object', function () {
 				should.exist(device.info);
-				device.info.ready.should.be.true;
-				device.info.type.should.be.a('string');
-				device.info.firmware.should.be.a('string');
+				device.info.should.have.property('build').which.is.a.String();
+				device.info.should.have.property('name').which.is.a.String();
+				device.info.should.have.property('ready').which.is.a.Boolean();
+				device.info.should.have.property('type').which.is.a.String();
+				device.info.should.have.property('version').which.is.a.String();
 			});
 			it('should be a BCS-460 or BCS-462', function () {
 				(device.info.type === 'BCS-460' || device.info.type === 'BCS-462').should.be.true;
 			});
-			it('should be using the correct dictionary', function () {
-				if (device.info.type === 'BCS-460') (require('../lib/dictionary-460') === device.dictionary).should.be.true;
-				if (device.info.type === 'BCS-462') (require('../lib/dictionary-462') === device.dictionary).should.be.true;
-			});
 		});
-		describe('of an inactive or unreachable device', function () {
+		describe('of an incorrect device address', function () {
 			var device;
 			
-			before(function (done) {
-				device = new Device('127.0.0.1', 80, done);
+			it('should return ECONNREFUSED Error', function (done) {
+				device = new Device('127.0.0.1', 80, function(e) {
+					should.exist(e);
+					e.should.have.property('code').which.equal('ECONNREFUSED');
+					done();
+				});
 			});
 			it('should have info.ready === false', function () {
-				should.exist(device.info);
 				device.info.ready.should.be.false;
 			});
 		});
 	});
-	describe('#read("network.currentIP")', function () {
+	describe('#read("network.ip")', function () {
 		var device;
 			
 		before(function (done) {
 			device = new Device(target.host, target.port, done);
 		});
 		it('should equal the test device ip', function (done) {
-			device.read('network.currentIP', function (e, res) {
+			device.read('network', function (e, doc) {
 				if (e) throw e;
-				res.should.equal(target.host);
+				doc.ip.join('.').should.equal(target.host);
 				done();
 			});
 		});
